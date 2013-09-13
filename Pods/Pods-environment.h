958a847282dd42003bea11fd5b0dd5200c81b34c
Silviu Pop <thesilviupop@gmail.com>
2013-09-13 15:08:19 +0300

    installed placekit

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,3 +12,15 @@
 #define COCOAPODS_VERSION_MINOR_MAObjCRuntime 0
 #define COCOAPODS_VERSION_PATCH_MAObjCRuntime 1
 
+// PlaceKit
+#define COCOAPODS_POD_AVAILABLE_PlaceKit
+#define COCOAPODS_VERSION_MAJOR_PlaceKit 1
+#define COCOAPODS_VERSION_MINOR_PlaceKit 0
+#define COCOAPODS_VERSION_PATCH_PlaceKit 0
+
+// PlaceKit/Core
+#define COCOAPODS_POD_AVAILABLE_PlaceKit_Core
+#define COCOAPODS_VERSION_MAJOR_PlaceKit_Core 1
+#define COCOAPODS_VERSION_MINOR_PlaceKit_Core 0
+#define COCOAPODS_VERSION_PATCH_PlaceKit_Core 0
+
