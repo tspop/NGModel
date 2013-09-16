c9e76eafed52af09c8fbc45adc838eeeee10f7a0
Silviu Pop <thesilviupop@gmail.com>
2013-09-16 13:53:52 +0300

    cocoa pods

---
+++ Pods/Pods-Tests-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GHUnitIOS
+#define COCOAPODS_POD_AVAILABLE_GHUnitIOS
+#define COCOAPODS_VERSION_MAJOR_GHUnitIOS 0
+#define COCOAPODS_VERSION_MINOR_GHUnitIOS 5
+#define COCOAPODS_VERSION_PATCH_GHUnitIOS 6
+
+// MAObjCRuntime
+#define COCOAPODS_POD_AVAILABLE_MAObjCRuntime
+#define COCOAPODS_VERSION_MAJOR_MAObjCRuntime 0
+#define COCOAPODS_VERSION_MINOR_MAObjCRuntime 0
+#define COCOAPODS_VERSION_PATCH_MAObjCRuntime 1
+
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 2
+#define COCOAPODS_VERSION_MINOR_OCMock 1
+#define COCOAPODS_VERSION_PATCH_OCMock 1
+
+// PlaceKit
+#define COCOAPODS_POD_AVAILABLE_PlaceKit
+#define COCOAPODS_VERSION_MAJOR_PlaceKit 1
+#define COCOAPODS_VERSION_MINOR_PlaceKit 0
+#define COCOAPODS_VERSION_PATCH_PlaceKit 0
+
+// PlaceKit/Core
+#define COCOAPODS_POD_AVAILABLE_PlaceKit_Core
+#define COCOAPODS_VERSION_MAJOR_PlaceKit_Core 1
+#define COCOAPODS_VERSION_MINOR_PlaceKit_Core 0
+#define COCOAPODS_VERSION_PATCH_PlaceKit_Core 0
+
